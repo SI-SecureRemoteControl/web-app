17c30d9f31d1854d72071f976f415f1300611adf
Ahmed Softic <asoftic1@etf.unsa.ba>
2025-04-01 23:37:43 +0200

    test

---
+++ backend/db.js
@@ -0,0 +1,51 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { up, database } = require("migrate-mongo");
+
+require('dotenv').config(); // Dodaj ovo na početak
+
+const isLocal = process.env.USE_LOCAL_DB === "true";
+const dbUri = isLocal ? process.env.DB_URI_LOCAL : process.env.DB_URI;
+
+console.log(dbUri);
+
+const client = new MongoClient(dbUri, {
+    tlsAllowInvalidCertificates: true,
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    //await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (error) {
+    console.error("Failed to connect:", error);
+  }
+}
+
+async function connectAndMigrate() {
+    try {
+        console.log("Connecting to MongoDB...");
+        
+        // Konekcija na bazu pomoću migrate-mongo
+        const { db } = await database.connect();
+
+        console.log("Running migrations...");
+        await up(db, client);
+        
+        console.log("Migrations applied!");
+    } catch (error) {
+        console.error("Error during migration:", error);
+    }
+}
+
+// Pokreni migracije
+run().then(() => {
+    connectAndMigrate();
+});

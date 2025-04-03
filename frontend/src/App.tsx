0f26e981b4cc2b87d4484e29171fc2bb7a5780bc
Imad <imad.huremovic@gmail.com>
2025-04-03 23:39:07 +0200

    add react-router, add login feature, add usefull packagews to frontend and backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css'
+import {Route, Routes} from "react-router-dom";
+import Login from "./pages/Login/Login.tsx";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute.tsx";
+import Home from "./pages/Home/Home.tsx";
 
 function App() {
 
   return (
-    <>
-    </>
+    <Routes>
+      <Route path="/login" element={<Login/>}/>
+      <Route element={<ProtectedRoute/>}>
+        <Route path="/" element={<Home/>}/>
+      </Route>
+    </Routes>
   )
 }
 

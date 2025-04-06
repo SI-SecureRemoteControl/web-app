34a34d53da482693abf286929414cc04c4b9f4f1
Imad <imad.huremovic@gmail.com>
2025-04-07 01:44:20 +0200

    fix routing in app, add redirect from home to dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,21 @@
 import './App.css';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login/Login.tsx';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute.tsx';
 import DeviceDashboard from './pages/Dashboard/DashboardPage.tsx';
 import Registration from "./pages/Registration/Registration.tsx";
+import Home from "./pages/Home/Home.tsx";
 
 function App() {
 
-    const token = localStorage.getItem('token'); 
-
     return (
         <Routes>
-            {/* Ako korisnik nije prijavljen, preusmerava na /login */}
-            <Route path="/" element={token ? <Navigate to="/dashboard" /> : <Navigate to="/login" />} />
             <Route path="/login" element={<Login />} />
-            {/* Zaštićene rute */}
             <Route element={<ProtectedRoute />}>
+                <Route path="/" element={<Home/>}/>
                 <Route path="/dashboard" element={<DeviceDashboard />} />
                 <Route path="/registration" element={<Registration />} />
             </Route>
-            {/* Catch-all za nepostojeće rute */}
-            <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     );
 }

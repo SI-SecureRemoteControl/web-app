207730437aacec8ce4305dba056944e87203b102
Adi Drakovac <129613464+adidrakovac1@users.noreply.github.com>
2025-04-06 22:16:21 +0200

    Fix route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,20 @@
 import './App.css';
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Login from './pages/Login/Login.tsx';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute.tsx';
-import Home from './pages/Home/Home.tsx';
+//import Home from './pages/Home/Home.tsx';
 import DeviceDashboard from './pages/Dashboard/DashboardPage.tsx';
-import { useEffect } from 'react';
 
 function App() {
-    const navigate = useNavigate();
-    useEffect(() => {
     const token = localStorage.getItem('token');
-    if (token) {
-        navigate('/dashboard');
-    } else {
-        navigate('/login');
-    }
-    }, [navigate]);
+
     return (
         <Routes>
             <Route path="/login" element={<Login />} />
+            <Route path="/" element={token ? <Navigate to="/dashboard" /> : <Navigate to="/login" />} />
             <Route element={<ProtectedRoute />}>
-                <Route path="/" element={<Home />} />
+                <Route path="/dashboard" element={<DeviceDashboard />} />
             </Route>
-            <Route path="/dashboard" element={<DeviceDashboard />} />
         </Routes>
     );
 }

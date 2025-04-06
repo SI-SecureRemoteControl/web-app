0ce2f89620bac5630a3e3e3feb758d6ca5d2808d
Adi Drakovac <129613464+adidrakovac1@users.noreply.github.com>
2025-04-06 22:59:16 +0200

    Fixing things

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,11 +4,13 @@ import Login from './pages/Login/Login.tsx';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute.tsx';
 //import Home from './pages/Home/Home.tsx';
 import DeviceDashboard from './pages/Dashboard/DashboardPage.tsx';
+import React from 'react';
 
 function App() {
     const token = localStorage.getItem('token');
 
     return (
+    <React.StrictMode>
         <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/" element={token ? <Navigate to="/dashboard" /> : <Navigate to="/login" />} />
@@ -16,6 +18,7 @@ function App() {
                 <Route path="/dashboard" element={<DeviceDashboard />} />
             </Route>
         </Routes>
+    </React.StrictMode>
     );
 }
 

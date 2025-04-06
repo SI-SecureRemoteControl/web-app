2f64db47c51cbf5c849ea349212a7e8d270904ad
Adi Drakovac <129613464+adidrakovac1@users.noreply.github.com>
2025-04-06 21:44:08 +0200

    Fix dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import Login from './pages/Login/Login.tsx';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute.tsx';
 import Home from './pages/Home/Home.tsx';
 import DeviceDashboard from './pages/Dashboard/DashboardPage.tsx';
+import { useEffect } from 'react';
 
 function App() {
+    const navigate = useNavigate();
+    useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+        navigate('/dashboard');
+    } else {
+        navigate('/login');
+    }
+    }, [navigate]);
     return (
         <Routes>
             <Route path="/login" element={<Login />} />
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,16 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions:{
+      output:{
+        manualChunks:undefined,
+      },
+    },
+  },
+  server:{
+    fs:{
+      strict: false,
+    },
+  },
 });

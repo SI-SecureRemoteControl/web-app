c48836d54f14d546d8f52d09458e38fa4d211383
Ahmed Softic <asoftic1@etf.unsa.ba>
2025-04-07 10:35:24 +0200

    navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,31 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Login from './pages/Login/Login.tsx';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute.tsx';
 import DeviceDashboard from './pages/Dashboard/DashboardPage.tsx';
 import Registration from "./pages/Registration/Registration.tsx";
 import Home from "./pages/Home/Home.tsx";
+import Navbar from "./components/Navbar/navbar.tsx"
 
 function App() {
+    const location = useLocation();
+    const isAuthenticated = !!localStorage.getItem('token'); // ili tvoja metoda
+
+    const hideNavbarOnRoutes = ['/login'];
+    const shouldShowNavbar = isAuthenticated && !hideNavbarOnRoutes.includes(location.pathname);
 
     return (
-        <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route element={<ProtectedRoute />}>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/dashboard" element={<DeviceDashboard />} />
-                <Route path="/registration" element={<Registration />} />
-            </Route>
-        </Routes>
+        <>
+            {shouldShowNavbar && <Navbar />}
+
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route element={<ProtectedRoute />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/dashboard" element={<DeviceDashboard />} />
+                    <Route path="/registration" element={<Registration />} />
+                </Route>
+            </Routes>
+        </>
     );
 }
 

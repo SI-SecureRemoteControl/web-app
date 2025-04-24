e18e1949deb9d237509225a8cc96a071d1511872
Adi Drakovac <129613464+adidrakovac1@users.noreply.github.com>
2025-04-24 12:37:30 +0200

    adding preview of user

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,11 +1,18 @@
 import { Link } from 'react-router-dom';
+import { LogOut } from 'lucide-react';
+import { jwtDecode } from 'jwt-decode';
 
 export interface navbarProps {
     handleLogout: () => void;
 }
 
-const Navbar = ({handleLogout}: navbarProps) => {
-
+const Navbar = ({ handleLogout }: navbarProps) => {
+    const token = localStorage.getItem('token'); 
+    let username = '';
+    if (token) {
+        const decoded: any = jwtDecode(token); 
+        username = decoded?.username; 
+    }
     return (
         <nav className="bg-gray-800 text-white px-6 py-4 shadow-md">
             <div className="flex justify-between items-center max-w-7xl mx-auto">
@@ -15,12 +22,20 @@ const Navbar = ({handleLogout}: navbarProps) => {
                     <Link to="/sessionview" className="hover:text-gray-300 transition">Sessions View</Link>
                     <Link to="/register-user" className="hover:text-gray-300 transition">Register Admin</Link>
                 </div>
-                <button
-                    onClick={handleLogout}
-                    className="bg-red-600 hover:bg-red-700 transition px-4 py-2 rounded text-sm"
-                >
-                    Logout
-                </button>
+
+                <div className="ml-auto flex items-center space-x-2"> 
+                    {username && (
+                        <div className="text-sm text-gray-300">
+                            Logged in as {username}
+                        </div>
+                    )}
+                    <button
+                        onClick={handleLogout}
+                        className="bg-red-600 hover:bg-red-700 transition px-4 py-2 rounded text-sm flex items-center"
+                    >
+                        <LogOut className="mr-2 h-5 w-5" /> 
+                    </button>
+                </div>
             </div>
         </nav>
     );
